ba671e550b66eb1e7afc6c83b6207e0906605d83
6304825149 <141232714+6304825149@users.noreply.github.com>
2023-08-21 13:14:02 +0530

    github new4 commit

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github new3 commit");
+        System.out.println( "github new4 commit");
     }
 }
